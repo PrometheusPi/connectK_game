bc4a0c837eea6aa45a6a21fc5bad763cf47c853b
Richard Pausch <r.pausch@hzdr.de>
2024-08-18 23:10:19 +0200

    add main file with description

---
+++ connect-k.py
@@ -0,0 +1,6 @@
+"""
+This game is inspired by a job interview test reported by Johannes Pausch.
+
+You have a (infinitly - here finite) horizontal field and try to connect k points.
+Each player takes turns to add their stones. In contrast to classical connect-4, stones are added beneeth existing stones and push up the stones of the collumn. 
+"""
